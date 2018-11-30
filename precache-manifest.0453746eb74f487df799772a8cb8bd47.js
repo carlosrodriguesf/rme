46f03148e9c2cccc3d624a741bcb12917b32a74d
Carlos Rodrigues <carlosrodriguesf96@gmail.com>
2018-11-30 08:40:25 -0200

    Updates

---
+++ precache-manifest.0453746eb74f487df799772a8cb8bd47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "ebfb49c4f1cb9a1e23e0",
+    "url": "/js/chunk-vendors.5de84b60.js"
+  },
+  {
+    "revision": "d6e0c74b3eab3229e0e2",
+    "url": "/js/app.44e5656a.js"
+  },
+  {
+    "revision": "c260e7f035f8486cadb3544b82e29f7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "271c08eb0dc17d42e07bf9213119fb06",
+    "url": "/img/user.271c08eb.png"
+  },
+  {
+    "revision": "ebfb49c4f1cb9a1e23e0",
+    "url": "/css/chunk-vendors.38039bdd.css"
+  },
+  {
+    "revision": "d6e0c74b3eab3229e0e2",
+    "url": "/css/app.1ffd8dd1.css"
+  }
+];
