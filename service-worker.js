f585be9e1ad415b64a7258056ffb83a69c2acd3b
Carlos Rodrigues <carlosrodriguesf96@gmail.com>
2018-12-01 21:24:31 -0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.004d4ff236ea592ac2431a7f3757404f.js"
+  "/rme/precache-manifest.6d18be5133f2a451355405d24c828d6f.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "remember.me.n"});
